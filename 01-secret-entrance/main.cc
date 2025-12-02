4094c9c1fcf4934aaff9f78eb6a4e533d66ad941
David Jonsson <david.jonsson306@gmail.com>
2025-12-02 13:04:50 +0100

    Day1 touchup, still not a solution to be proud of

--- 01-secret-entrance/main.cc
+++ 01-secret-entrance/main.cc
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <print>
-#include <ranges>
 #include <string>
 
 #include "util.h"
 
 using namespace std;
 
-int get_number(const string& s) { return stoi(s.substr(1)) * (s[0] == 'L' ? -1 : 1); }
-
 int main(int argc, char* argv[]) {
     int  x = 50, part1 = 0, part2 = 0;
-    auto turn = [&part2](int x, int dx) {
-        int change = dx < 0 ? -1 : 1;
-        for (int i = 0; i < abs(dx); ++i) {
-            x += change;
+    auto turn = [&part2](int x, string line) {
+        int dx = line[0] == 'L' ? -1 : 1;
+        for (int i = 0; i < stoi(line.substr(1)); ++i) {
+            x += dx;
             if (x == 100) {
                 x = 0;
             } else if (x < 0) {
@@ -24,9 +21,8 @@ int main(int argc, char* argv[]) {
         }
         return x;
     };
-    for (int dx : read_lines(cin) | vs::transform(get_number)) {
-        x = turn(x, dx);
-        if (x == 0) { part1++; }
+    for (string line : read_lines(cin)) {
+        if ((x = turn(x, line)) == 0) { part1++; }
     }
     println("Part1: {}", part1);
     println("Part2: {}", part2);
