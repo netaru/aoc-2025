73a67900eeade4cd5e999f3b76649758063b409d
David Jonsson <david.jonsson306@gmail.com>
2025-12-05 20:45:55 +0100

    Day 05 touchup

--- 05-cafeteria/main.cc
+++ 05-cafeteria/main.cc
@@ -11,14 +11,15 @@ using namespace std;
 
 struct lohi {
     i64 low, high;
-    lohi(i64 _low, i64 _high) : low(_low), high(_high) {}
+    lohi(vector<i64> in) : low(in[0]), high(in[1]) {}
     lohi(const lohi &other) : low(other.low), high(other.high) {}
     bool inside(i64 n) const { return n >= low and n <= high; }
     i64 count() const { return high - low + 1; }
-    bool overlap(const lohi &other) { return inside(other.low) or inside(other.high); }
-
-    void new_low(i64 n) { low = min(low, n); }
-    void new_high(i64 n) { high = max(high, n); }
+    bool overlap(const lohi &other) const { return inside(other.low) or inside(other.high); }
+    void merge(const auto &other) {
+        low = min(low, other.low);
+        high = max(high, other.high);
+    }
 };
 
 vector<lohi> merge(vector<lohi> in) {
@@ -27,39 +28,25 @@ vector<lohi> merge(vector<lohi> in) {
     for (first = in.begin(); first < in.end(); first = second) {
         second = first + 1;
         lohi current(*first);
-        for (; second < in.end() and current.overlap(*second); ++second) {
-            current.new_low(second->low);
-            current.new_high(second->high);
-        }
+        for (; second < in.end() and current.overlap(*second); ++second) { current.merge(*second); }
         result.push_back(current);
     }
     return result;
 }
 
 vector<lohi> parse(vector<string> input) {
-    vector<lohi> result;
-    for (auto &s : input) {
-        vector<i64> current = ints<i64>(s);
-        result.emplace_back(current[0], current[1]);
-    }
-    return merge(dave::sort(result, [](auto lhs, auto rhs) { return lhs.low < rhs.low; }));
+    vector<lohi> intervals = input | vs::transform([](string s) { return lohi(ints<i64>(s)); }) | rs::to<vector>();
+    return merge(dave::sort(intervals, [](auto lhs, auto rhs) { return lhs.low < rhs.low; }));
 }
 
 int main(int argc, char *argv[]) {
     vector<string> input = split(read(cin), "\n\n");
-    vector<i64> values = ints<i64>(input[1]);
     vector<lohi> intervals = parse(split(dave::replace(input[0], "-", " "), "\n"));
-
-    i64 part1 = 0;
-    for (const auto &v : values) {
-        for (const auto &i : intervals) {
-            if (i.inside(v)) {
-                part1++;
-                break;
-            }
-        }
-    }
-    println("Part1: {}", part1);
+    auto in_any_range = [&intervals](i64 n) {
+        return rs::find_if(intervals, [&n](lohi obj) { return obj.inside(n); }) != intervals.end();
+    };
+    auto is_fresh = [&in_any_range](i64 v) -> i64 { return in_any_range(v) ? 1 : 0; };
+    println("Part1: {}", rs::fold_left(ints<i64>(input[1]) | vs::transform(is_fresh), 0ll, plus<i64>()));
     println("Part2: {}", rs::fold_left(intervals | vs::transform([](auto i) { return i.count(); }), 0ll, plus<i64>()));
     return 0;
 }
