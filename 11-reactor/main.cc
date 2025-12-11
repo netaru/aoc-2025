29d46f5a61e4d1d21df5a3c88a65cccf0ab0c5cb
David Jonsson <david.jonsson306@gmail.com>
2025-12-11 18:24:35 +0100

    Day 11 rework
    Use the same function for part1 and part2

--- 11-reactor/main.cc
+++ 11-reactor/main.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <print>
 #include <unordered_map>
 #include <vector>
 
@@ -6,54 +8,40 @@
 
 using namespace std;
 
-using simple_cache_t = unordered_map<size_t, i64>;
-using cache_t = unordered_map<size_t, array<i64, 4>>;
+using cache_t = map<tuple<size_t, size_t>, i64>;
 using edges_t = unordered_map<size_t, vector<size_t>>;
 
 struct reactor {
     vector<string> nodes;
-    array<size_t, 5> xs;
     edges_t edges;
     cache_t cache;
-    simple_cache_t simple;
-
-    size_t add(string s) {
-        size_t u = nodes.size();
-        nodes.emplace_back(s);
-        cache[u] = { -1, -1, -1, -1 };
-        return u;
-    }
 
     size_t find(const string &s) {
         if (auto iter = rs::find(nodes, s); iter != nodes.end()) { return distance(nodes.begin(), iter); }
-        return add(s);
+        nodes.emplace_back(s);
+        return nodes.size() - 1;
     }
 
     reactor(istream &is) {
         for (auto e : read_lines(is) | vs::transform([](auto s) { return split(dave::remove(s, ':')); }))
             rs::for_each(e | vs::drop(1), [&, i = find(e[0])](auto s) { edges[i].push_back(find(s)); });
-        xs = { find("you"), find("svr"), find("dac"), find("fft"), find("out") };
     }
 
-    size_t part1(size_t current) {
-        if (simple.contains(current)) { return simple[current]; }
-        if (current == xs[4]) { return simple[current] = 1; }
-        return simple[current] =
-                       rs::fold_left(edges[current] | vs::transform([&](auto n) { return part1(n); }), 0, plus<>());
+    i64 solve(pair<size_t, size_t> p) {
+        if (cache.contains(p)) { return cache[p]; }
+        if (p.first == p.second) { return 1; }
+        return cache[p] = rs::fold_left(
+                       edges[p.first] | vs::transform([&](auto n) { return solve({ n, p.second }); }), 0, plus<>());
     }
 
-    int nstate(size_t n, int state) { return state | (n == xs[2] ? 1 : 0) | (n == xs[3] ? 2 : 0); }
-    size_t part2(size_t current, int state = 0) {
-        if (cache[current][state] != -1) { return cache[current][state]; }
-        if (current == xs[4]) { return cache[current][state] = static_cast<i64>(state == 3); }
-        return cache[current][state] = rs::fold_left(
-                       edges[current] | vs::transform([&](auto n) { return part2(n, nstate(n, state)); }), 0, plus<>());
-    }
+    i64 solve(string from, string to) { return solve({ find(from), find(to) }); }
 };
 
 int main(int argc, char *argv[]) {
     reactor r(cin);
-    println("Part1: {}", r.part1(r.xs[0]));
-    println("Part2: {}", r.part2(r.xs[1]));
+    i64 path1 = r.solve("svr", "fft") * r.solve("fft", "dac") * r.solve("dac", "out");
+    i64 path2 = r.solve("svr", "dac") * r.solve("dac", "fft") * r.solve("fft", "out");
+    println("Part1: {}", r.solve("you", "out"));
+    println("Part2: {}", path1 + path2);
     return 0;
 }
